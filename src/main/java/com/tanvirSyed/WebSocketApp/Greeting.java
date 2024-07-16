0b66818b4b17f2efae2a177b8874b62d05a7043a
Tanvir Alam Syed <tanviralamsyed@gmail.com>
2024-07-16 16:07:25 +0100

    Initial commit

---
+++ src/main/java/com/tanvirSyed/WebSocketApp/Greeting.java
@@ -0,0 +1,18 @@
+package com.tanvirSyed.WebSocketApp;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
---
+++ src/main/java/com/tanvirSyed/WebSocketApp/WebSocketAppApplication.java
@@ -0,0 +1,13 @@
+package com.tanvirSyed.WebSocketApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketAppApplication.class, args);
+	}
+
+}
